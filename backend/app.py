80948976937c6521eb60a153b03458befc64628b
Trambitskiy Yura <tr.yura@yahoo.com>
2015-11-22 22:45:20 +0300

    Format app init

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='../templates', static_folder='../frontend')
+from flask import Flask, render_template, jsonify
+app = Flask(
+	__name__, 
+	template_folder='../templates', 
+	static_folder='../frontend'
+)
 
 @app.route("/")
 def index():
